2bde41ff42235a1e356563337ab7b6097eb280f8
fade <dimabreusgo@gmail.com>
2024-05-31 21:20:21 +0300

    music off and on added

--- Assets/Scenes/UI/Settings.cs
+++ Assets/Scenes/UI/Settings.cs
@@ -1,19 +1,54 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes.UI
 {
     public class Settings : MonoBehaviour
     {
+        [SerializeField] private AudioSource audioSourceMusic;
+
+        [SerializeField] private Toggle musicToggle;
+        [SerializeField] private Toggle audioToggle;
+        private bool _isAudioEnabled;
 
         public void OnSettingsPressed()
         {
-            Debug.Log(1);
             gameObject.SetActive(true);
         }
+
         public void OnExitPressed()
         {
             gameObject.SetActive(false);
         }
 
+        private void OnToggleMusic(bool value)
+        {
+            if (value)
+            {
+                audioSourceMusic.Play();
+            }
+            else
+            {
+                audioSourceMusic.Stop();
+            }
+        }
+
+        private void OnToggleAudio(bool value)
+        {
+            _isAudioEnabled = value;
+        }
+        private void OnEnable()
+        {
+            musicToggle.onValueChanged.AddListener(OnToggleMusic);
+            audioToggle.onValueChanged.AddListener(OnToggleAudio);
+        }
+
+        private void OnDisable()
+        {
+            musicToggle.onValueChanged.RemoveListener(OnToggleMusic);
+            audioToggle.onValueChanged.RemoveListener(OnToggleAudio);
+
+        }
     }
 }
